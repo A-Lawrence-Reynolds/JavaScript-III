687a1a607b5c8856c9aa3bd8c1b55a2fcd5fc9af
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-08-21 16:13:20 -0700

    Did this>js assignment and started on the prototype game.

--- assignments/this.js
+++ assignments/this.js
@@ -2,25 +2,51 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. 
+Window binding, "THIS" is references the window/global .
 * 2. 
+Implicit binding, only works with objects that have methods on them.
 * 3. 
+New bining, constructs a new object and "this" references it
 * 4. 
+Explicit binding, it occurs when .call() .apply() and .bind() are used.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
-
+function theWorld() {
+    console.log( /*this*/ );
+}
+theWorld()
 // Principle 2
-
 // code example for Implicit Binding
+let myCow = {
+    name: "Ribeye",
+    moo: "MoooHooAH",
+    cow: function () {
+        console.log(this.moo);
+    }
+}
+myCow.cow();
+
 
 // Principle 3
-
 // code example for New Binding
+function Pig(saying) {
+    this.thing = saying;
+}
+let myPig = new Pig("I like to snort");
+
+console.log(myPig.thing);
 
 // Principle 4
-
 // code example for Explicit Binding
+function wing() {
+    console.log(this.iDo);
+}
+let myWing = {
+    name: "Wing",
+    iDo: " I create lift"
+}
+wing.call(myWing);
